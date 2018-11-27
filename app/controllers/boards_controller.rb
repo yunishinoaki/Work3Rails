e3a711d7ca8159d3d5d581ac70f373205ef783b5
Yu Nishinoaki <ruka.tech.ny@gmail.com>
2018-11-27 10:10:18 +0900

    RailsUdemy1

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,53 @@
+class BoardsController < ApplicationController
+  before_action :set_target_board ,only: %i[show edit update destroy]
+
+  def index
+    @boards = Board.page(params[:page])
+  end
+
+  def new
+    @board = Board.new
+  end
+
+  def create
+    board = Board.new(board_params)
+    if board.save
+      flash[:notice] = "「#{board.title}」の掲示板を作成しました"
+      redirect_to board
+    else
+      redirect_to new_board_path, flash: {
+        board: board,
+        error_messages: board.errors.full_messages
+      }
+    end
+  end
+
+  def show
+    @comment = @board.comments.new
+  end
+
+  def edit
+  end
+
+  def update
+    board.update(board_params)
+
+    redirect_to board
+  end
+
+  def destroy
+    @board.delete
+    redirect_to boards_path,flash: { notice: "「#{@board.title}」の掲示板が削除されました"}
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:name, :title, :body)
+  end
+
+  def set_target_board
+    @board = Board.find(params[:id])
+  end
+
+end
---
+++ app/models/board.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id         :bigint(8)        not null, primary key
+#  name       :string(255)
+#  title      :string(255)
+#  body       :text(65535)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Board < ApplicationRecord
+  has_many :comments
+  validates :name, presence: true, length: { maximum:10}
+  validates :title, presence: true, length: { maximum: 30}
+  validates :body, presence: true, length: { maximum: 1000} 
+end
